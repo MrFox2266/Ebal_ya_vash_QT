0be506c1fc5db27a173fe4411905f41806e0e071
MrFox2266 <foxmr288@gmail.com>
2025-11-30 05:38:30 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 
 using namespace std;
+
 void makeTable(double a, double b, double h, ofstream& file) {
 	cout << fixed << setprecision(4);
 	file << fixed << setprecision(4);
@@ -13,12 +14,14 @@ void makeTable(double a, double b, double h, ofstream& file) {
 	cout << "----------------\n";
 	file << "----------------\n";
 
-	for (double x = a; x <= b; x += h){
-		if (fabs(sin(x)) < 0.001 || fabs(cos(x)) < 0.001){
-			cout << x << "\tne opredeleno\n";
-			file << x << "\tne opredeleno\n";
+	for (double x = a; x <= b; x += h) {
+		// Проверяем особые точки (где sin(x)=0 или cos(x)=0)
+		if (fabs(sin(x)) < 0.001 || fabs(cos(x)) < 0.001) {
+			cout << x << "\tне опр.\n";
+			file << x << "\tне опр.\n";
 		}
-		else{
+		else {
+		// Вычисляем функцию: y(x) = sqrt(ctg(x)*tg(x))
 			double ctg = cos(x) / sin(x);
 			double tg = sin(x) / cos(x);
 			double y = sqrt(ctg * tg);
@@ -29,46 +32,38 @@ void makeTable(double a, double b, double h, ofstream& file) {
 }
 
 int main() {
+	setlocale(LC_ALL, "Russian");
+
 	double a, b, h;
 
-	cout << "Vvedite nachalo intervala a: ";
+	cout << "Введите начало интервала a: ";
 	cin >> a;
-	cout << "Vvedite konec intervala b: ";
+	cout << "Введите конец интервала b: ";
 	cin >> b;
-	cout << "Vvedite shag h: ";
+	cout << "Введите шаг h: ";
 	cin >> h;
 
-	// Проверяем, что ввод прошел успешно
-	if (cin.fail()) {
-		cout << "Oshibka vvoda! Vvedite chisla." << endl;
-		return 1;
-	}
-
 	if (h <= 0) {
-		cout << "Oshibka: shag dolzhen byt polozhitelnym!" << endl;
+		cout << "Ошибка: шаг должен быть положительным!" << endl;
 		return 1;
 	}
 
 	if (a > b) {
-		cout << "Oshibka: nachalo dolzhno byt menshe konca!" << endl;
-		cout << "a = " << a << ", b = " << b << endl; // Покажем что ввел пользователь
+		cout << "Ошибка: начало должно быть меньше конца!" << endl;
 		return 1;
 	}
 
 	ofstream file("result.txt");
 
 	if (!file.is_open()) {
-		cout << "Oshibka: ne udalos sozdat fail!" << endl;
+		cout << "Ошибка: не удалось создать файл!" << endl;
 		return 1;
 	}
 
 	makeTable(a, b, h, file);
 	file.close();
 
-	cout << "Rezultaty sohraneny v fail result.txt" << endl;
-	cout << "Najmite Enter dlya vyhoda...";
-	cin.ignore();
-	cin.get();
+	cout << "Результаты сохранены в файл result.txt" << endl;
 
 	return 0;
 }
